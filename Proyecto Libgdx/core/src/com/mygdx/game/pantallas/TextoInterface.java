b4d5614a476dbdd3a94bcd095bc27e3dda029491
KendrickLapark <jose.exposito002a@gmail.com>
2020-06-17 13:32:42 +0200

    Timer
    Añadida una cuenta atras con el tiempo del nivel

--- Proyecto Libgdx/core/src/com/mygdx/game/pantallas/TextoInterface.java
+++ Proyecto Libgdx/core/src/com/mygdx/game/pantallas/TextoInterface.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
+import java.text.DecimalFormat;
+
 public class TextoInterface {
 
     private static BitmapFont bitmapFont1 = new BitmapFont();
@@ -14,6 +16,7 @@ public class TextoInterface {
 
     private static float tiempo;
 
+    public static int tiempototal;
 
 
     public static void SetSpriteBatch(SpriteBatch batch){
@@ -31,14 +34,24 @@ public class TextoInterface {
     }*/
 
     public static void draw (OrthographicCamera camara){
+
+        tiempototal = 20;
+
         tiempo += Gdx.graphics.getDeltaTime();
         double tiemp = Math.ceil(tiempo);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#");
+
+        tiempototal -=tiemp;
+
         Vector3 vector3 = new Vector3(20,40,0);
         camara.unproject(vector3);
         bitmapFont1.getData().scaleX = 0.25f;
         bitmapFont1.getData().scaleY = 0.25f;
         bitmapFont1.setUseIntegerPositions(false);
 
-        bitmapFont1.draw(spriteBatch, "Tiempo: "+tiemp , vector3.x, vector3.y);
+        bitmapFont1.draw(spriteBatch, "Tiempo: "+tiempototal , vector3.x, vector3.y);
+
+
     }
 }
